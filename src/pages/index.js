accabdf505a3233e4d0f63e1f52b33985506b1d7
Shawn Cook <sfncook@gmail.com>
2024-01-15 11:19:13 -0800

    MetricsGrid

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,11 @@ import { Box, CircularProgress, Container, Unstable_Grid2 as Grid } from '@mui/m
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 import { useApi } from '../hooks/use-api';
 import { OverviewConversationsByDate } from '../sections/overview/overview-conversations-by-date';
+import { MetricsGrid } from '../sections/metrics/metrics-grid';
 
 const now = new Date();
 
 const Page = () => {
-  const {countPerDayByCompanyId, msgCountPerDayByCompanyId, loading} = useApi();
-
-  const seriesData = Object.keys(countPerDayByCompanyId).map(companyId=>{
-    return {
-      name: companyId,
-      data: Object.values(countPerDayByCompanyId[companyId])
-    }
-  })
-  const seriesData_msgCountPerDayByCompanyId = Object.keys(msgCountPerDayByCompanyId).map(companyId=>{
-    return {
-      name: companyId,
-      data: Object.values(msgCountPerDayByCompanyId[companyId])
-    }
-  })
-  // console.log(seriesData)
-
   return (
     <>
       <Head>
@@ -30,43 +15,7 @@ const Page = () => {
           Sales Chatbot
         </title>
       </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8
-        }}
-      >
-        {loading && <CircularProgress />}
-        <Container maxWidth="xl">
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              xs={12}
-              lg={8}
-            >
-
-              <OverviewConversationsByDate
-                chartSeries={seriesData}
-                sx={{ height: '100%' }}
-                title={'New Conversations per Date'}
-              />
-            </Grid>
-            <Grid
-              xs={12}
-              lg={8}
-            >
-              <OverviewConversationsByDate
-                chartSeries={seriesData_msgCountPerDayByCompanyId}
-                sx={{ height: '100%' }}
-                title={'New Message per Date'}
-              />
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
+      <MetricsGrid />
     </>
   )
 };
---
+++ src/sections/metrics/metrics-grid.js
@@ -0,0 +1,63 @@
+import { Box, CircularProgress, Container, Unstable_Grid2 as Grid } from '@mui/material';
+import { OverviewConversationsByDate } from '../overview/overview-conversations-by-date';
+import { useApi } from '../../hooks/use-api';
+
+export const MetricsGrid = (props) => {
+  const {countPerDayByCompanyId, msgCountPerDayByCompanyId, loading} = useApi();
+
+  const seriesData = Object.keys(countPerDayByCompanyId).map(companyId=>{
+    return {
+      name: companyId,
+      data: Object.values(countPerDayByCompanyId[companyId])
+    }
+  })
+  const seriesData_msgCountPerDayByCompanyId = Object.keys(msgCountPerDayByCompanyId).map(companyId=>{
+    return {
+      name: companyId,
+      data: Object.values(msgCountPerDayByCompanyId[companyId])
+    }
+  })
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      {loading && <CircularProgress />}
+      <Container maxWidth="xl">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            xs={12}
+            lg={8}
+          >
+
+            <OverviewConversationsByDate
+              chartSeries={seriesData}
+              sx={{ height: '100%' }}
+              title={'New Conversations per Date'}
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            lg={8}
+          >
+            <OverviewConversationsByDate
+              chartSeries={seriesData_msgCountPerDayByCompanyId}
+              sx={{ height: '100%' }}
+              title={'New Message per Date'}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
+};
+
+MetricsGrid.protoTypes = {
+};
